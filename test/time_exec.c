e0800ad8d87a27b969b55ecec9f181ef29f2c039
Harry Presman <hpresman@cs.ucsb.edu>
2011-05-07 00:49:16 -0700

    dont care any more

--- test/time_exec.c
+++ test/time_exec.c
@@ -1,12 +1,18 @@
 #include <unistd.h>
 #include <limits.h>
+#include <stdio.h>
+#include <sys/wait.h>
 
 int main(){
-    for( int i = 0; i < INT_MAX; i += 2 ){
+    printf("%d\n", INT_MAX );
+    for( int i = 0; i < 10; i += 2 ){
         i--;
         int pid = fork();
-        if( pid ){
-            int ret = execl( "time_exec_in", (char *) 0 );
+        if( !pid ){
+            int ret = execle( "time_exec_in", (char *) 0 );
+        }else{
+            int status;
+            waitpid( pid, &status, 0 );
         }
     }
 }
